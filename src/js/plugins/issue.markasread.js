b0e50dff6835bb853e5be115fc3d3a118d8c9b2d
Herman van Rink <rink@initfour.nl>
2013-12-03 10:38:19 +0100

    Copy pasted version from feature/mark-all-as-read-1317740 branch, from before the split in separate files

--- src/js/plugins/issue.markasread.js
+++ src/js/plugins/issue.markasread.js
@@ -20,3 +20,33 @@ Drupal.behaviors.dreditorIssueMarkAsRead = {
     });
   }
 };
+
+/**
+ * Attach mark ALL as read to project issue tables.
+ */
+Drupal.behaviors.dreditorMarkAllAsRead = function (context) {
+  $('table.project-issue', context).once('dreditor-issuemarkallasread', function () {
+    $tmp = $('<a href="#" class="dreditor-mark-all-as-read">Mark ALL as read</a>');
+    $tmp.click(function () {
+      $('.marker').each(function () {
+        var $marker = $(this);
+        var $link = $marker.prev('a');
+        $.ajax({
+          // The actual HTML page output is irrelevant, so denote that by using
+          // the appropriate HTTP method.
+          type: 'HEAD',
+          url: $link.attr('href'),
+          complete: function () {
+            $marker.remove();
+          }
+        });
+      });
+      // Refresh the page to get rid of all updated / x new markup.
+      // TODO: this should happen after all requests from above are finished...
+      //location.reload(true);
+    });
+
+    // Insert after the pager
+    $('#feeds').after($tmp);
+  });
+};
